1a09f66a6d3a817878ea5af6e755688aeeb4ea54
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2021-06-28 23:45:48 -0300

    Ajuste na API para diminuir o tempo de resposta

--- src/store/modules/partner/reducer.js
+++ src/store/modules/partner/reducer.js
@@ -3,12 +3,22 @@ import produce from 'immer';
 const INITIAL_STATE = {
   loading: false,
   partners: [],
+  partner: {},
 };
 export default function partner(state = INITIAL_STATE, action) {
   switch (action.type) {
     case '@partner/ADD_REQUEST':
       return produce(state, (draft) => {
         draft.loading = true;
+      });
+    case '@partner/LOAD_REQUEST':
+      return produce(state, (draft) => {
+        draft.loading = true;
+      });
+    case '@partner/LOAD_SUCCESS':
+      return produce(state, (draft) => {
+        draft.partner = action.payload;
+        draft.loading = false;
       });
     case '@partner/UPDATE_REQUEST':
       return produce(state, (draft) => {
