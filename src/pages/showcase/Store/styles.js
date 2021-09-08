3b5c6e1ce173f2c63ebd384e6106b17fe8dc4590
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2021-09-07 23:48:34 -0300

    Criação do carrosel

--- src/pages/showcase/Store/styles.js
+++ src/pages/showcase/Store/styles.js
@@ -29,6 +29,79 @@ export default makeStyles((theme) => ({
     margin: theme.spacing(0.2),
     textAlign: 'center',
   },
+  carousel: {
+    margin: '20px 0',
+  },
+  carouselItem: {
+    height: 340,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  carouselAdvertisementItem: {
+    height: 340,
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  carouselAdvertisementImg: {
+    objectFit: 'contain',
+    width: '100%',
+    maxWidth: 1000,
+    maxHeight: 450,
+  },
+  carouselProduct: {
+    flex: 0.48,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    cursor: 'pointer',
+    maxHeight: 450,
+    '& img': {
+      maxWidth: 250,
+      maxHeight: 250,
+      [theme.breakpoints.down('sm')]: {
+        maxWidth: 150,
+        maxHeight: 150,
+      },
+      [theme.breakpoints.down('xs')]: {
+        maxWidth: 80,
+        maxHeight: 80,
+      },
+    },
+  },
+  carouselProductInfo: {
+    padding: '10px 10px 10px 20px',
+    width: '100%',
+    heigth: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  carouselProductName: {
+    fontSize: 26,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    display: '-webkit-box',
+    '-webkit-line-clamp': 2,
+    '-webkit-box-orient': 'vertical',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: 15,
+    },
+  },
+  carouselProductPrice: {
+    fontSize: 36,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: 20,
+    },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 10,
+    },
+  },
   partnerAvatar: {
     width: theme.spacing(7),
     height: theme.spacing(7),
