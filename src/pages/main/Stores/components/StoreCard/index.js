5813820c2b58c058a9560b0657f7dae1acca6e35
BrunoDevF <brunohenriquex00@gmail.com>
2022-06-22 22:04:18 -0300

    feat(storecard): criado componente de card da loja

---
+++ src/pages/main/Stores/components/StoreCard/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//components
+import {
+    Grid,
+} from '@material-ui/core';
+import { Link } from 'react-router-dom';
+// styles
+import useStyle from './styles';
+//icons
+import { BsArrowRightShort } from 'react-icons/bs'
+
+export function StoreCard({ store }) {
+    const classes = useStyle();
+    
+    return (
+        <Grid item xs={6} lg={4} className={classes.grid}>
+            <Link to={`loja/${store.url}`} className={classes.storeCard}>
+                <img
+                    src={store.logo ? store.logo.url : ''}
+                    alt={store.name}
+                    className={classes.imgStore}
+                />
+                <div className={classes.bodyCard}>
+                    <label>{store.name}</label>
+                    <span>Endereço: {store.address}</span>
+                    <button>Visitar <BsArrowRightShort /></button>
+                </div>
+            </Link>
+        </Grid>
+    );
+}
+
+StoreCard.propTypes = {
+    // eslint-disable-next-line react/forbid-prop-types
+    store: PropTypes.object.isRequired,
+};
---
+++ src/pages/main/Stores/components/StoreCard/styles.js
@@ -0,0 +1,89 @@
+import { makeStyles } from '@material-ui/core';
+import { darken } from 'polished';
+
+export default makeStyles((theme) => ({
+    grid: {
+        margin: '.2rem',
+        background: '#FFF',
+        borderRadius: '7px',
+        boxShadow: '3px 5px 14px -2px rgba(0,0,0,0.46)'
+        
+    },
+    storeCard: {
+        height: 370,
+        
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        cursor: 'pointer',
+        borderRadius: '5px',
+        color: '#000',
+        transition: 'all  .2s',
+        backgroundColor: darken(0.02, '#fff'),
+        '&:hover': {
+            transform: 'scale(1.1)',
+            boxShadow: '3px 5px 14px -2px rgba(0,0,0,0.46)',
+        },
+        '& img': {
+            width: '100%',
+            maxHeight: '70%',
+            minWidth: '70%',
+            minHeight: '70%',
+            borderTopLeftRadius: '5px',
+            borderTopRightRadius: '5px'
+        },
+        '& h3': {
+            fontSize: 14,
+        },
+        '& button': {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+
+            '& svg': {
+                fontSize: '2rem'
+            },
+
+            background: '#ED2F55',
+            border:'none',
+            padding: '0.8rem 1.4rem',
+            color: '#fff',
+            borderRadius: '4px',
+            marginTop: '4px',
+
+            fontSize: '1rem',
+            lineHeight: '1rem',
+            letterSpacing: '2px',
+            fontWeight: '600',
+            height: '3rem',
+        },
+        
+    },
+    bodyCard: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+
+        height: '30%',
+        width: '100%',
+        padding: '.5rem',
+        '& label': {
+            fontSize: '1.2rem',
+            lineHeight: '1rem',
+            fontWeight: '600',
+            color: 'rgb(66, 70, 77)',
+            height: '3rem',
+            marginBottom: '2px'
+        },
+        '& span': {
+            fontSize: '.95rem',
+            lineHeight: '1rem',
+            color: 'rgb(66, 70, 77)',
+            height: '3rem',
+        }
+    },
+    imgStore: {
+        width: '100%',
+    }
+}));
