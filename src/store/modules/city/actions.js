1e5f1a3640454dab9c09dee4fbe9e8b299720ea3
BrunoDevF <brunohenriquex00@gmail.com>
2022-07-14 21:46:15 -0300

    feat: criado actions, reducer e saga para buscar lista de cidades

--- src/store/modules/city/actions.js
+++ src/store/modules/city/actions.js
@@ -3,6 +3,17 @@ export function loadCitiesRequest() {
     type: '@city/LIST_CITIES_REQUEST',
   };
 }
+export function loadCitiesActiveRequest() {
+  return {
+    type: '@city/LIST_CITIES_ACTIVE_REQUEST',
+  };
+}
+export function loadCitiesActiveSuccess(cities) {
+  return {
+    type: '@city/LOAD_CITIES_ACTIVE_SUCCESS',
+    payload: cities,
+  };
+}
 export function loadCitiesSuccess(cities) {
   return {
     type: '@city/LOAD_CITIES_SUCCESS',
