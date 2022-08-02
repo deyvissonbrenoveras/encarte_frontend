d507f01d8a728ea71af07e4727d274b9f1fb7fb1
BrunoDevF <brunohenriquex00@gmail.com>
2022-08-02 20:06:22 -0300

    feat: listando e atualizando lista ao adicionar nova categoria

--- src/pages/dashboard/StoreCategory/index.js
+++ src/pages/dashboard/StoreCategory/index.js
@@ -1,22 +1,29 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import AddButton from '../../../components/AddButton';
-import { loadStoresRequest } from '../../../store/modules/store/actions';
 import LoadingIcon from '../../../components/LoadingIcon';
 import PrivilegeEnum from '../../../util/PrivilegeEnum';
 import CustomTable from '../../../components/CustomTable';
 
+import api from '../../../services/api';
+
 function StoreCategory() {
-  const dispatch = useDispatch();
-  const stores = useSelector((state) => state.store.stores);
-  const loading = useSelector((state) => state.store.loading);
+
+  const [storeCategory, setStoreCategory] = useState([])
+  const [loading, setLoading] = useState(true);
+  
   const profile = useSelector((state) => state.profile.profile);
+
   useEffect(() => {
     async function getStores() {
-      dispatch(loadStoresRequest(true));
+      const response = await api.get('/store-categories');
+      setStoreCategory(response.data);
+      setLoading(false);
     }
-    getStores();
-  }, [dispatch]);
+    if(loading) {
+      getStores();
+    }
+  }, [loading]);
   return (
     <>
       {profile.privilege <= PrivilegeEnum.SYSTEM_ADMINISTRATOR && (
@@ -41,18 +48,11 @@ function StoreCategory() {
               disablePadding: false,
               label: 'Nome',
               type: 'link',
-            },
-            {
-              id: 'url',
-              numeric: false,
-              disablePadding: false,
-              label: 'Url',
-            },
+            }
           ]}
-          rows={stores.map((store) => ({
-            id: store.id,
-            name: { href: `/updatestore/${store.id}`, label: store.name },
-            url: store.url,
+          rows={storeCategory.map((category) => ({
+            id: category.id,
+            name: { href: `/updatestore/${category.id}`, label: category.name },
           }))}
         />
       )}
